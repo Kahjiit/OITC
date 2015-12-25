4c2b0b8b68498ce2828676d1cda7975a39225acd
Mats <matsgemmeke@gmail.com>
2015-12-26 00:24:31 +0100

    Update GameListener.java

--- me/kahjiit/OITC/listeners/GameListener.java
+++ me/kahjiit/OITC/listeners/GameListener.java
@@ -1,18 +1,31 @@
 package me.kahjiit.OITC.listeners;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
 
 import me.kahjiit.OITC.OITC;
 import me.kahjiit.OITC.arena.Arena;
@@ -22,6 +35,7 @@ import me.kahjiit.OITC.arena.State;
 public class GameListener implements Listener {
 
 	OITC plugin;
+	private int kills;
 	
 	public GameListener() {
 		this.plugin = (OITC) OITC.getPlugin();
@@ -40,8 +54,8 @@ public class GameListener implements Listener {
 							event.setCancelled(true);
 							return;
 						}
-						player.getWorld().createExplosion(player.getLocation(), 0F);
 						event.setDamage(30);
+						arrow.remove();
 					}
 				}
 			}
@@ -72,16 +86,30 @@ public class GameListener implements Listener {
 		}
 	}
 	
+	@EventHandler (priority = EventPriority.LOW)
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player player = event.getPlayer();
+		if (Manager.isPlaying(player)) {
+			event.setCancelled(true);
+			Manager.getArena(player).sendArenaMessage(OITC.prefix + ChatColor.WHITE + player.getName() + ": " + ChatColor.GRAY + event.getMessage());
+		}
+
+	}
+	
 	@EventHandler
 	public void onCommandSend(PlayerCommandPreprocessEvent event) {
-		if (!Manager.isPlaying(event.getPlayer())) {
+		Player player = event.getPlayer();
+		if (player.hasPermission("oitc.admin")) {
 			return;
 		}
-		if (Manager.getArena(event.getPlayer()).getState() == State.INGAME) {
-			if (event.getMessage().startsWith("/oitc")) {
+		if (!Manager.isPlaying(player)) {
+			return;
+		}
+		if (Manager.getArena(player).getState() == State.INGAME) {
+			if (event.getMessage().startsWith("/oitc") || event.getMessage().startsWith("/msg")) {
 				return;
 			}
-			event.getPlayer().sendMessage(ChatColor.RED + "You may not use commands while playing OITC!");
+			player.sendMessage(ChatColor.RED + "You may not use commands while playing OITC!");
 			event.setCancelled(true);
 		}
 	}
@@ -90,6 +118,68 @@ public class GameListener implements Listener {
 	public void onDrop(PlayerDropItemEvent event) {
 		if (Manager.isPlaying(event.getPlayer())) {
 			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onFallDamage(EntityDamageEvent event) {
+		if (event.getEntity() instanceof Player) {
+			Player player = (Player) event.getEntity();
+			if (event.getCause() == DamageCause.FALL) {
+				if (Manager.isPlaying(player)) {
+					event.setCancelled(true);
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		if (Manager.isPlaying(player)) {
+			Arena arena = Manager.getArena(player);
+			if (arena.isRunning()) {
+				event.getDrops().clear();
+				event.setDeathMessage("");
+				event.setDroppedExp(0);
+				player.getWorld().createExplosion(player.getLocation(), 0F);
+				player.getKiller().setHealth(20);
+				player.getInventory().clear();
+				if (event.getEntity().getLastDamageCause().getCause() == DamageCause.ENTITY_ATTACK) {
+					onPlayerKill(player, player.getKiller(), "stabbed");
+				}
+				if (event.getEntity().getLastDamageCause().getCause() == DamageCause.PROJECTILE) {
+					onPlayerKill(player, player.getKiller(), "shot");
+				}
+ 			}
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		if (Manager.isPlaying(event.getPlayer())) {
+			if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+				event.setCancelled(true);
+			}
+		}
+	}
+	
+	public void onPlayerKill(Player player, Player killer, String reason) {
+		Arena arena = Manager.getArena(player);
+		arena.sendArenaMessage(OITC.prefix + ChatColor.RED + player.getName() + ChatColor.AQUA + " has been " + reason
+				+ " by " + ChatColor.RED + killer.getName() + ChatColor.AQUA + "!");
+		
+		arena.addArrow(killer);
+		
+		Scoreboard sb = killer.getScoreboard();
+		Score score = sb.getObjective(DisplaySlot.SIDEBAR).getScore(killer.getName());
+		kills = score.getScore() + 1;
+		score.setScore(kills);
+		if (kills >= 20) {
+			arena.sendArenaMessage(OITC.prefix + ChatColor.AQUA + ChatColor.STRIKETHROUGH.toString() + "=============================================");
+			arena.sendArenaMessage(OITC.prefix + ChatColor.GOLD + killer.getName() + " has reached 20 kills and has won the game.");
+			arena.sendArenaMessage(OITC.prefix + ChatColor.AQUA + ChatColor.STRIKETHROUGH.toString() + "=============================================");
+			arena.stop();
 		}
 	}
 	
@@ -107,6 +197,13 @@ public class GameListener implements Listener {
 	}
 	
 	@EventHandler
+	public void onPlayerPickupItem(PlayerPickupItemEvent event) {
+		if (event.getItem().getItemStack().getType() == Material.ARROW) {
+			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
 		if (Manager.isPlaying(player)) {
@@ -114,6 +211,19 @@ public class GameListener implements Listener {
 			arena.removePlayer(player);
 			Manager.removePlayer(player);
 			arena.sendArenaMessage(OITC.prefix + player.getName() + " has left the game.");
+		}
+	}
+	
+	@EventHandler
+	public void onRespawn(PlayerRespawnEvent event) {
+		Player player = event.getPlayer();
+		if (!Manager.isPlaying(player)) {
+			return;
+		}
+		Arena arena = Manager.getArena(player);
+		if (arena.isRunning()) {
+			event.setRespawnLocation(arena.getNextSpawn());
+			arena.setDefaultInventory(player);
 		}
 	}
 }
