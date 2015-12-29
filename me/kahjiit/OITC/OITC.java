b576603dc21f19c98f9517d4f2260e9fb211757b
Mats <matsgemmeke@gmail.com>
2015-12-29 10:56:28 +0100

    Update OITC.java

--- me/kahjiit/OITC/OITC.java
+++ me/kahjiit/OITC/OITC.java
@@ -2,8 +2,12 @@ package me.kahjiit.OITC;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
 import me.kahjiit.OITC.arena.Arena;
 import me.kahjiit.OITC.arena.Manager;
@@ -17,7 +21,7 @@ public class OITC extends JavaPlugin {
 	public GameListener gl = new GameListener();
 	public SignListener sl = new SignListener();
 	
-	public static String prefix = "[" + ChatColor.GOLD + ChatColor.BOLD.toString() + "OITC" + ChatColor.RESET + "] " + ChatColor.GRAY;
+	public static String prefix = "[" + ChatColor.GOLD + "OITC" + ChatColor.RESET + "] " + ChatColor.GRAY;
 	
 	public void onEnable() {
 		Settings.getInstance().init(this);
@@ -38,5 +42,19 @@ public class OITC extends JavaPlugin {
 	
 	public static Plugin getPlugin() {
 		return Bukkit.getServer().getPluginManager().getPlugin("OITC");
+	}
+	
+	public static void setMainScoreboard(Player player) {
+		Scoreboard msb = Bukkit.getScoreboardManager().getNewScoreboard();
+		Objective objective = msb.registerNewObjective(ChatColor.GOLD + "OITC Lobby", "oitc");
+		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+		objective.getScore(ChatColor.YELLOW + "Kills: " + ChatColor.WHITE + Settings.getInstance().getStat(player, "Kills")).setScore(5);
+		objective.getScore(ChatColor.YELLOW + "Deaths: " + ChatColor.WHITE + Settings.getInstance().getStat(player, "Deaths")).setScore(4);
+		objective.getScore(ChatColor.GOLD + "KDR: " + ChatColor.WHITE + Settings.getInstance().getKDR(player)).setScore(3);
+		objective.getScore(ChatColor.YELLOW + "Games: " + ChatColor.WHITE + Settings.getInstance().getStat(player, "Games")).setScore(2);
+		objective.getScore(ChatColor.YELLOW + "Wins: " + ChatColor.WHITE + Settings.getInstance().getStat(player, "Wins")).setScore(1);
+		objective.getScore(ChatColor.GOLD + "WGR: " + ChatColor.WHITE + Settings.getInstance().getWGR(player)).setScore(0);
+		
+		player.setScoreboard(msb);
 	}
 }	
