4335f7bcbcd5ed47467c6b131e0a4d8be995f262
Mats <matsgemmeke@gmail.com>
2015-12-29 10:57:58 +0100

    Update GameListener.java

--- me/kahjiit/OITC/listeners/GameListener.java
+++ me/kahjiit/OITC/listeners/GameListener.java
@@ -1,7 +1,9 @@
 package me.kahjiit.OITC.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,6 +21,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -31,15 +34,14 @@ import me.kahjiit.OITC.OITC;
 import me.kahjiit.OITC.arena.Arena;
 import me.kahjiit.OITC.arena.Manager;
 import me.kahjiit.OITC.arena.State;
+import me.kahjiit.OITC.misc.Settings;
+import net.minecraft.server.v1_8_R3.PacketPlayInClientCommand;
+import net.minecraft.server.v1_8_R3.PacketPlayInClientCommand.EnumClientCommand;
 
 public class GameListener implements Listener {
 
-	OITC plugin;
 	private int kills;
-	
-	public GameListener() {
-		this.plugin = (OITC) OITC.getPlugin();
-	}
+	int id;
 	
 	@EventHandler
 	public void onArrowHit(EntityDamageByEntityEvent event) {
@@ -49,14 +51,12 @@ public class GameListener implements Listener {
 				Player player = (Player) event.getEntity();
 				Player shooter = (Player) arrow.getShooter();
 				if (Manager.isPlaying(player) && Manager.isPlaying(shooter)) {
-					if (Manager.getArena(player).isRunning()) {
-						if (player == shooter) {
-							event.setCancelled(true);
-							return;
-						}
-						event.setDamage(30);
-						arrow.remove();
+					if (player == shooter) {
+						event.setCancelled(true);
+						return;
 					}
+					event.setDamage(30);
+					arrow.remove();
 				}
 			}
 		}
@@ -122,11 +122,14 @@ public class GameListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onFallDamage(EntityDamageEvent event) {
+	public void onPlayerDamage(EntityDamageEvent event) {
 		if (event.getEntity() instanceof Player) {
 			Player player = (Player) event.getEntity();
-			if (event.getCause() == DamageCause.FALL) {
-				if (Manager.isPlaying(player)) {
+			if (Manager.isPlaying(player)) {
+				if (Manager.getArena(player).getState() == State.WAITING) {
+					event.setCancelled(true);
+				}
+				if (event.getCause() == DamageCause.FALL) {
 					event.setCancelled(true);
 				}
 			}
@@ -137,21 +140,25 @@ public class GameListener implements Listener {
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		Player player = event.getEntity();
 		if (Manager.isPlaying(player)) {
-			Arena arena = Manager.getArena(player);
-			if (arena.isRunning()) {
-				event.getDrops().clear();
-				event.setDeathMessage("");
-				event.setDroppedExp(0);
-				player.getWorld().createExplosion(player.getLocation(), 0F);
-				player.getKiller().setHealth(20);
-				player.getInventory().clear();
-				if (event.getEntity().getLastDamageCause().getCause() == DamageCause.ENTITY_ATTACK) {
-					onPlayerKill(player, player.getKiller(), "stabbed");
+			event.getDrops().clear();
+			event.setDeathMessage("");
+			event.setDroppedExp(0);
+			player.getWorld().createExplosion(player.getLocation(), 0F);
+			player.getKiller().setHealth(20);
+			player.getInventory().clear();
+			
+			id = Bukkit.getScheduler().scheduleSyncDelayedTask(OITC.getPlugin(), new Runnable() {
+				public void run() {
+					((CraftPlayer) player).getHandle().playerConnection.a(new PacketPlayInClientCommand(EnumClientCommand.PERFORM_RESPAWN));
 				}
-				if (event.getEntity().getLastDamageCause().getCause() == DamageCause.PROJECTILE) {
-					onPlayerKill(player, player.getKiller(), "shot");
-				}
- 			}
+			}, 10L);
+			
+			if (event.getEntity().getLastDamageCause().getCause() == DamageCause.ENTITY_ATTACK) {
+				onPlayerKill(player, player.getKiller(), "stabbed");
+			}
+			if (event.getEntity().getLastDamageCause().getCause() == DamageCause.PROJECTILE) {
+				onPlayerKill(player, player.getKiller(), "shot");
+			}
 		}
 	}
 	
@@ -164,23 +171,63 @@ public class GameListener implements Listener {
 		}
 	}
 	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		OITC.setMainScoreboard(player);
+		Settings.getInstance().setPlayerStats(player);
+		player.sendMessage(OITC.prefix + ChatColor.AQUA + "Please notice OITC is currently in Beta. Bugs will occur more frequently and stats may get wiped.");
+	}
+	
 	public void onPlayerKill(Player player, Player killer, String reason) {
 		Arena arena = Manager.getArena(player);
-		arena.sendArenaMessage(OITC.prefix + ChatColor.RED + player.getName() + ChatColor.AQUA + " has been " + reason
-				+ " by " + ChatColor.RED + killer.getName() + ChatColor.AQUA + "!");
+		
+		arena.getStats(killer).addKill();
+		arena.getStats(killer).addStreak();
+		arena.getStats(player).addDeath();
+		
+		arena.sendArenaMessage(OITC.prefix + ChatColor.RED + player.getName() + quickStats(arena, player) + ChatColor.AQUA + " has been " + reason
+				+ " by " + ChatColor.RED + killer.getName() + quickStats(arena, killer) + ChatColor.AQUA + " !");
+		
+		if (arena.getStats(player).getStreak() > 0) {
+			if (arena.getStats(player).getStreak() >= 5) {
+				arena.sendArenaMessage(OITC.prefix + ChatColor.RED + killer.getName() + ChatColor.AQUA
+						+ " has broken the killstreak of " + ChatColor.RED + player.getName() + ChatColor.AQUA + "!");
+			}
+			arena.getStats(player).resetStreak();
+		}
+		if (arena.getStats(killer).getStreak() >= 3) {
+			arena.sendArenaMessage(OITC.prefix + ChatColor.RED + killer.getName() + ChatColor.AQUA + " is on a " + ChatColor.GOLD
+					+ arena.getStats(killer).getStreak() + ChatColor.AQUA + " killstreak!");
+		}
 		
 		arena.addArrow(killer);
 		
 		Scoreboard sb = killer.getScoreboard();
-		Score score = sb.getObjective(DisplaySlot.SIDEBAR).getScore(killer.getName());
-		kills = score.getScore() + 1;
-		score.setScore(kills);
+		
+		if (killer.hasPermission("oitc.perks")) {
+			Score score = sb.getObjective(DisplaySlot.SIDEBAR).getScore(ChatColor.GREEN + killer.getName());
+			kills = score.getScore() + 1;
+			score.setScore(kills);
+		}
+		else {
+			Score score = sb.getObjective(DisplaySlot.SIDEBAR).getScore(killer.getName());
+			kills = score.getScore() + 1;
+			score.setScore(kills);
+		}
+		
 		if (kills >= 20) {
 			arena.sendArenaMessage(OITC.prefix + ChatColor.AQUA + ChatColor.STRIKETHROUGH.toString() + "=============================================");
 			arena.sendArenaMessage(OITC.prefix + ChatColor.GOLD + killer.getName() + " has reached 20 kills and has won the game.");
 			arena.sendArenaMessage(OITC.prefix + ChatColor.AQUA + ChatColor.STRIKETHROUGH.toString() + "=============================================");
 			arena.stop();
 		}
+	}
+	
+	private String quickStats(Arena arena, Player player) {
+		String s = ChatColor.WHITE + " [" + ChatColor.GREEN + arena.getStats(player).getKills() + ChatColor.WHITE + "/" + ChatColor.RED
+				+ arena.getStats(player).getDeaths() + ChatColor.WHITE + "]";
+		return s;
 	}
 	
 	@EventHandler
@@ -190,7 +237,7 @@ public class GameListener implements Listener {
 			return;
 		}
 		if (Manager.getArena(player).getState() == State.STARTING) {
-			if (event.getTo().getX() != event.getFrom().getX() && event.getTo().getZ() != event.getFrom().getZ()) {
+			if (event.getTo().getX() != event.getFrom().getX() || event.getTo().getZ() != event.getFrom().getZ()) {
 				player.teleport(player.getLocation());
 			}
 		}
@@ -221,9 +268,7 @@ public class GameListener implements Listener {
 			return;
 		}
 		Arena arena = Manager.getArena(player);
-		if (arena.isRunning()) {
-			event.setRespawnLocation(arena.getNextSpawn());
-			arena.setDefaultInventory(player);
-		}
+		event.setRespawnLocation(arena.getNextSpawn());
+		arena.setDefaultInventory(player);
 	}
 }
